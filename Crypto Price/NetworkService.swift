6aa87ffe0ebe5b75deb1223233bd397f8e9e44b1
Andrei <kavsha007@mail.ru>
2022-09-09 14:12:14 +0300

    Initial Commit

---
+++ Crypto Price/NetworkService.swift
@@ -0,0 +1,13 @@
+//
+//  NetworkService.swift
+//  Crypto Price
+//
+//  Created by Andrew Kvasha on 09.09.2022.
+//
+
+import Foundation
+
+
+final class NetworkService {
+    static let shared = NetworkService()
+}
---
+++ Crypto Price/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Crypto Price
+//
+//  Created by Andrew Kvasha on 09.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor.red
+        title = "HI"
+    }
+
+
+}
+
